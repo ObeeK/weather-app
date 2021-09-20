4fbf6d6f382a650f7ef4e3bbd6af7d1b37d57847
ObeeK <chasenharvey@gmail.com>
2021-09-19 23:27:42 -0500

    last push

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,6 +1,12 @@
 // var cityName = document.querySelector
 var citySearch = document.querySelector("#weather-search")
 var searchButton = document.querySelector("#searchButton")
+var forecast = document.querySelector("#forecast")
+// var day2 = document.querySelector("#day2")
+// var day3 = document.querySelector("#day3")
+// var day4 = document.querySelector("#day4")
+// var day5 = document.querySelector("#day5")
+
 console.log(citySearch);
 // Call current weather API for lat/lon
 var weatherFunction = function (cityName) {
@@ -21,7 +27,9 @@ var weatherFunction = function (cityName) {
                 })
                 .then(function (oneCallData) {
                     console.log({ oneCallData })
+                    renderItems(oneCallData)
                     // this is where we'll display weather info on page
+                    // use bootstrap button for UV index
                 })
 
         })
@@ -38,6 +46,46 @@ var cityName = citySearch.value;
 weatherFunction(cityName);
 })
 
+var renderItems = function(data){
+    // render Current Weather
+    
+    renderForecast(data.daily, data.timezone)
+}
+
+var renderForecast = function(dailyForecast, timeZone) {
+    for(var i = 1; i < 6; i++) {
+        var col = document.createElement("div")
+        var card = document.createElement("div")
+        var cardBody = document.createElement("div")
+        var icon = document.createElement("img")
+        var tempEl = document.createElement("p")
+        var windEl = document.createElement("p")
+        var humidityEl = document.createElement("p")
+        var cardTitle = document.createElement("h5")
+
+        col.append(card)
+        card.append(cardBody)
+        cardBody.append(cardTitle, icon, tempEl, windEl, humidityEl)
+
+        col.setAttribute("class", "col-md")
+        col.classList.add("fiveDayCard")
+        card.setAttribute("class", "card bg-primary h-100 text-white")
+        cardBody.setAttribute("class", "card-body p-2")
+        cardTitle.classList.add("card-title")
+        tempEl.classList.add("card-text")
+        windEl.classList.add("card-text")
+        humidityEl.classList.add("card-text")
+        
+        var currentDate = dayjs().format('MM/DD/YYYY');
+        cardTitle.innerHTML = currentDate
+
+        var temp = dailyForecast.daily.temp.day
+        tempEl.innerHTML = temp;
+        
+        forecast.append(col)
+        
+    }
+}
 // localStorage/previously searched cities
 
 // when previous cities are clicked, how do we search it?
